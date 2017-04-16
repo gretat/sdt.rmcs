11ee609423c37f5394cf003dc4842fddab33f583
gretat <2028282t@student.gla.ac.uk>
2017-04-16 14:22:02 +0100

    update description

--- R/sdt.data.R
+++ R/sdt.data.R
@@ -1,23 +1,28 @@
 #' Create data for one sample
 #'
-#' This creates a dataset with 1000 participants to be used with the main function \code{\link{rate.statistics}}.
-#'
+#' Creates a dataset with 1000 participants to be used with the main function \code{\link{rate.statistics}}.
+#' The parameters can be changed or it can be ran with the defaults by calling sdt.data()
 #'
 #' @param tot.signal A number - The total number of signals i.e. blue dots that have to be detected Default = 85
 #' @param tot.lure A number - The total number of lures i.e. number green dots that should not be responded to, Default = 100
-#' @param min.hits A number - Minimum ammount of hits. Dafalt to 0.5
-#' @param min.cr A number - Minimum ammount of correct rejections.  Dafalt to 0.6
+#' @param min.hits A number - Minimum proportion of hits. Dafalt to 0.5
+#' @param min.cr A number - Minimum proportion of correct rejections.  Dafalt to 0.6
 #'
+#'@return
+#'A data table with the following columns
+#'\itme{hits}{total number of hits per participant randomly chosen between tot.signal and the minimum proportion hits specified}
+#'\itme{corRej}{total number of correct rejection per participant randomly chosen between tot.lure and the minimum proportion critical rejections specified}
+#'\itme{miss}{total number of misses per participant - left from the total signals after removing hits}
+#'\itme{falarm}{total number of false alarms per participant - left from the total lures after removing correct rejections}
 #'@examples
-#'\dontrun{
 #'sdt.data(tot.signal = 85, tot.lure = 100, min.hits = 0.5, min.cr = 0.6)
-#'}
+#'
 
 sdt.data <- function(tot.signal = 85, tot.lure = 100, min.hits = 0.5, min.cr = 0.6){
 
   mh <- round((tot.signal*min.hits), digits = 0)
   mcr <- round((tot.lure*min.cr), digits = 0)
-  sdt <- data.frame(ID = rep(1:1000, each=1), hits = sample(c(mh:tot.signal), size = 1000, replace = T),
+  sdt <- data.frame(hits = sample(c(mh:tot.signal), size = 1000, replace = T),
                     corRej = sample(c(mcr:tot.lure), size = 1000, replace = T))
   sdt$miss   <- (tot.signal - sdt$hits)
   sdt$falarm <- (tot.lure - sdt$corRej)
